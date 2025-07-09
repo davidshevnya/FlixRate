d4be3ca09f63ffdff2b82456441bea7304f0b9d8
davidshevnya <davidshevnya@ukr.net>
2025-07-09 11:25:23 +0300

    Created Movie Model

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Genre
+from .models import Genre, Movie
 
 class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
 admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+
--- movies/models.py
+++ movies/models.py
@@ -10,5 +10,22 @@ class Genre(models.Model):
     def __str__(self):
         return self.title
     
+class Movie(models.Model):
+    movie_id = models.IntegerField()
+    title = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    overview = models.TextField()
+    genre = models.ManyToManyField(Genre)
+    popularity = models.FloatField()
+    poster = models.URLField(null=True, blank=True)
+    video = models.URLField(null=True, blank=True)
+    release_date = models.DateField()
+    language = models.TextField(max_length=100, null=True, blank=True)
+    vote_averate = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Title: {self.title}'
     
-    
+    class Meta:
+        ordering = ('-release_date',)
