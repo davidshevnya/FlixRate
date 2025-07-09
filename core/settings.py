21dd2b2c65fb1304c5f11f1c559230bda8b21861
davidshevnya <davidshevnya@ukr.net>
2025-07-09 10:42:32 +0300

    Flixrate folder renamed to core

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'flixrate.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'flixrate.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
