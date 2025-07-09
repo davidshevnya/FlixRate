6713ed7926f4b08db5add35444ccb6ff6667444d
davidshevnya <davidshevnya@ukr.net>
2025-07-09 10:36:31 +0300

    Created Movies Genre Model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre
+
+class GenreAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Genre, GenreAdmin)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Genre(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    
+    
