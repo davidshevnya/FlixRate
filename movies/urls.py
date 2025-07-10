75a0daa9ab30fd8cf1ec98d6bb3e8f85a0f67aae
davidshevnya <davidshevnya@ukr.net>
2025-07-10 15:55:20 +0300

    Create Templates files

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("movies/<slug:slug>/", views.detail, name="detail")
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Movie
 
 
 def home(request):
-    return render(request, "movies/home.html")
+    movies = Movie.objects.order_by("-release_date")[:9]
+    context = {"movies": movies}
+    return render(request, "movies/home.html", context)
+
+
+def detail(request, slug):
+    movie = get_object_or_404(Movie, slug=slug)
+    return render(request, "movies/detail.html", context={"movie": movie})
